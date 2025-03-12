24ec7bdfabd96735a945978893f069fc48fd5709
AbdelrhmanMuhammed173 <sabdo6227@gmail.com>
2025-03-12 14:27:34 +0200

    Update B. Bear and Finding Criminals.cpp

--- codeforces/B. Bear and Finding Criminals.cpp
+++ codeforces/B. Bear and Finding Criminals.cpp
@@ -3,39 +3,47 @@
 using namespace std;
  
 int main() {
-    int n,a;
-    
+    int n, a;   
     cin >> n >> a;
-    vector<int>arr(n);
-    for (int i = 0;i < n;++i) cin >> arr[i];
-    int x = n;
-    int s;
+    vector<int> arr(n); 
+    for (int i = 0; i < n; ++i) cin >> arr[i];
+
+    int x = n; // Number of cities
+    int s; // Counter for caught criminals
+
+    // Check if there is a criminal in Limak's starting position
     if (arr[a - 1] == 1) s = 1;
     else s = 0;
-    int i = a-1, j = a, k = a - 2;
+
+    int i = a - 1, j = a, k = a - 2; // Initialize pointers for checking both sides
+
+    // Iterate through cities symmetrically around Limak's position
     while (x--) {
         if (k != -1 && j != n) {
-            if (arr[k] == 1 && arr[j] == 1) s+=2;
- 
+            // If both symmetric cities have criminals, count both
+            if (arr[k] == 1 && arr[j] == 1) s += 2;
         }
         else {
-            break;
+            break; 
         }
         k--;
         j++;
     }
+
+    // If the left side is fully checked, process remaining right-side cities
     if (k == -1) {
-        for (int i = j;i < n;++i) {
+        for (int i = j; i < n; ++i) {
             if (arr[i] == 1) s++;
         }
     }
+    // If the right side is fully checked, process remaining left-side cities
     else if (j == n) {
-        for (int i = k;i >= 0;i--) {
+        for (int i = k; i >= 0; i--) {
             if (arr[i] == 1) s++;
         }
     }
+
     cout << s;
- 
- 
+
     return 0;
 }
